1f606719820ded21aba7baabe7eebfc408d07e5b
Blake Han <han.sangyeup@gmail.com>
2021-04-07 23:45:54 -0400

    button styling

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -10,9 +10,11 @@ const GlobalStyles = createGlobalStyle`
     body{
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size: 14px;
+        background-color: black;
     }
     *{
         box-sizing: border-box;
+        color: white
     }
 `;
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Ul = styled.ul`
+  display: flex;
+`;
+const List = styled.li`
+  margin: 1.2em 2em;
+  background-color: #56776c;
+  font-family: "Roboto", sans-serif;
+  padding: 0.5em 1.2em;
+  border-radius: 2em;
+  width: 5vw;
+  text-align: center;
+  &:hover {
+    background-color: #464f41;
+    cursor: pointer;
+  }
+`;
 
 const Header = () => {
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/tv">TV</Link>
-      <Link to="/search">Search</Link>
-      <Link to="/detail">Detail</Link>
+      <Ul>
+        <List>
+          <Link to="/">Home</Link>
+        </List>
+        <List>
+          <Link to="/tv">TV</Link>
+        </List>
+        <List>
+          <Link to="/search">Search</Link>
+        </List>
+        <List>
+          <Link to="/detail">Detail</Link>
+        </List>
+      </Ul>
     </>
   );
 };
