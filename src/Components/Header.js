455029cb00d56abd5a7a035245e5f701c41e1f9e
Blake Han <han.sangyeup@gmail.com>
2021-04-08 23:54:53 -0400

    Add withRouter to use props

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,43 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
 const Ul = styled.ul`
   display: flex;
 `;
 const List = styled.li`
-  margin: 1.2em 2em;
-  background-color: #56776c;
+  margin: 1.2em 1.4em;
   font-family: "Roboto", sans-serif;
-  padding: 0.5em 1.2em;
-  border-radius: 2em;
-  width: 5vw;
+  padding: 0.7em;
+  width: 6vw;
   text-align: center;
+
+  border-bottom: 0.3em solid ${props => (props.current ? "red" : "transparent")};
   &:hover {
     background-color: #464f41;
     cursor: pointer;
   }
+  transition: border-bottom 0.5s ease-in-out;
 `;
 
-const Header = () => {
-  return (
-    <>
-      <Ul>
-        <List>
-          <Link to="/">Home</Link>
-        </List>
-        <List>
-          <Link to="/tv">TV</Link>
-        </List>
-        <List>
-          <Link to="/search">Search</Link>
-        </List>
-        <List>
-          <Link to="/detail">Detail</Link>
-        </List>
-      </Ul>
-    </>
-  );
-};
+const Header = ({ location: { pathname } }) => (
+  <>
+    {console.log(pathname)}
+    <Ul>
+      <List current={pathname === "/"}>
+        <Link to="/">Home</Link>
+      </List>
+      <List current={pathname === "/tv"}>
+        <Link to="/tv">TV</Link>
+      </List>
+      <List current={pathname === "/search"}>
+        <Link to="/search">Search</Link>
+      </List>
+      <List current={pathname === "/detail"}>
+        <Link to="/detail">Detail</Link>
+      </List>
+    </Ul>
+  </>
+);
 
-export default Header;
+export default withRouter(Header);
