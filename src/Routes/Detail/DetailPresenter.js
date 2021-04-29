f4b839130e884555fa7c139befabe8bae992a87b
Blake Han <han.sangyeup@gmail.com>
2021-04-28 20:27:49 -0400

    Add Description to Detail Presenter

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -6,36 +6,70 @@ import Message from "../../Components/Message";
 
 const BASE_URL = "https://image.tmdb.org/t/p/original/";
 const Container = styled.div`
-  position: absolute;
+  position: relative;
   width: 100vw;
-  height: 90vh;
+  height: calc(100vh - 4.5rem);
   background-color: grey;
+  padding: 3rem;
 `;
 const BackPoster = styled.div`
-  position: relative;
+  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   background-image: url(${props => props.back});
   background-position: center center;
-
   background-size: cover;
+  filter: blur(3px);
   opacity: 0.4;
+  z-index: 0;
 `;
-const Grid = styled.div``;
+const Content = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
 const Poster = styled.div`
-  width: 20rem;
-  height: 27rem;
+  width: 30%;
+  min-width: 20rem;
+  height: 100%;
   background-image: url(${props => props.poster});
-  background-position: center;
+  background-position: center center;
+  background-size: contain;
   background-repeat: no-repeat;
-  background-size: cover;
+  z-index: 1;
+  border-radius: 0.4rem;
+  flex-wrap: wrap;
 `;
 const Description = styled.div`
-  color: white;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 2rem 2rem;
+  padding-left: 4rem;
+  font-size: 1.5rem;
+  flex-wrap: wrap;
+  > span {
+    margin-bottom: 1.5rem;
+  }
 `;
-const Genre = styled.div``;
+
+const Title = styled.span`
+  font-size: 3rem;
+`;
+const Year = styled.span``;
+const Genre = styled.span`
+  font-size: 1rem;
+`;
+const Runtime = styled.span``;
+const Vote = styled.span``;
+const Overview = styled.span`
+  width: 100%;
+  font-size: 1rem;
+`;
 
 const DetailPresenter = ({ detail, isLoading, error, isMovie }) =>
   isLoading ? (
@@ -44,10 +78,31 @@ const DetailPresenter = ({ detail, isLoading, error, isMovie }) =>
     <>
       <Container>
         <BackPoster back={BASE_URL + detail.backdrop_path} />
-        <Grid>
+        <Content>
           <Poster poster={BASE_URL + detail.poster_path}></Poster>
-          <Description>Hi</Description>
-        </Grid>
+          <Description>
+            <Title>
+              {detail.original_title}
+              <Year> ({detail.release_date.slice(0, 4)})</Year>
+            </Title>
+            <Genre>
+              {detail.genres.map((genre, index) =>
+                index > 0 ? ` ∙ ` + genre.name : genre.name
+              )}
+              &nbsp;&nbsp;&nbsp;
+              <Runtime>
+                {detail.runtime >= 60
+                  ? `${Math.floor(detail.runtime / 60)}h ${Math.floor(
+                      detail.runtime % 60
+                    )}m`
+                  : `${detail.runtime % 60}m`}
+              </Runtime>
+            </Genre>
+
+            <Vote>⭐️{detail.vote_average} / 10</Vote>
+            <Overview>{detail.overview}</Overview>
+          </Description>
+        </Content>
 
         {error && error.length > 0 && <Message text={error} color="#bbbbbb" />}
       </Container>
